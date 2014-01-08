4e47d94a1b139aba2955531baebe508b26dc6c51
Jürgen Bickert <juergenbickert@gmail.com>
2014-01-09 00:56:31 +0100

    fix documentation name

--- conveyer-belt.lisp
+++ conveyer-belt.lisp
@@ -17,7 +17,7 @@
   (setf (svref (cdr belt) (setf (car belt) (the fixnum (1- (the fixnum (car belt)))))) thing))
 
 (defun pick (belt offset)
-  "pick the last recent element from the element"
+  "pick the last recent element from the front of the belt"
   (declare (optimize (speed 3) (safety 0))
            (type fixnum offset))
   (svref (cdr belt)
